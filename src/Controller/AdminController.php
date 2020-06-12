8439ae73fe2c0ed71bfd9689a21e90e6faa94b2c
zazalit <lisa.24@hotmail.fr>
2020-06-12 10:49:20 +0200

    tableau administration_users

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
+/**
+ * @Route("/admin", name="admin")
+ * @IsGranted("ROLE_ADMIN")
+ */
 class AdminController extends AbstractController
 //Login is the home page
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/", name="admin")
      */
+
+    //request to display the table of candidates
     public function index()
     {
-        return $this->render('Admin/index.html.twig', ['page_name' => 'Admin_Home']);
+        $user = $this->getDoctrine()->getRepository(User::class)
+            ->findAll();
+        return $this->render('Admin/index.html.twig', ['page_name' => 'Candidats', 'user' => $user]);
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -51,6 +51,11 @@ class User implements UserInterface
      */
     private $isReady = false;
 
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $is_admin;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -161,6 +166,18 @@ class User implements UserInterface
     public function setIsReady(bool $isReady): self
     {
         $this->isReady = $isReady;
+
+        return $this;
+    }
+
+    public function getIsAdmin(): ?bool
+    {
+        return $this->is_admin;
+    }
+
+    public function setIsAdmin(bool $is_admin): self
+    {
+        $this->is_admin = $is_admin;
 
         return $this;
     }
