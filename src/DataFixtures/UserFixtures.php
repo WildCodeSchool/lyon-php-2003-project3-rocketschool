4096e1c216437176761a843f27df4de847abc096
galgros <galgros@yahoo.fr>
2020-06-15 19:20:39 +0200

    add fixtures for users and video

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoder;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setEmail('user@mail.com')
+            ->setPassword($this->passwordEncoder->encodePassword($user, 'password'))
+            ->setFirstname('Robert')
+            ->setLastname('Lafondu');
+        $manager->persist($user);
+
+        $admin = new User();
+        $admin->setEmail('admin@mail.com')
+            ->setPassword($this->passwordEncoder->encodePassword($admin, 'adminpassword'))
+            ->setRoles(["ROLE_ADMIN"])
+            ->setFirstname('Jhonny')
+            ->setLastname('Begood');
+        $manager->persist($admin);
+
+        $manager->flush();
+    }
+}
