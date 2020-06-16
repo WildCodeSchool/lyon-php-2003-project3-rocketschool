a610fd3f61f8c32d957fa28f82f090e23b960018
Nico <n.gaget@gmail.com>
2020-06-16 18:12:26 +0200

    gedmo

--- src/Form/QuestionType.php
+++ src/Form/QuestionType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 class QuestionType extends AbstractType
 {
