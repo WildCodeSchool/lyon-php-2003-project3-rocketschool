4ecb10d6077270c7de013e6f1c948ad01b748929
Nico <n.gaget@gmail.com>
2020-07-03 17:29:28 +0200

    crud question / proposition

---
+++ src/Form/PropositionType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Proposition;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PropositionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Proposition'
+            ])
+            ->add('isGood')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Proposition::class,
+        ]);
+    }
+}
