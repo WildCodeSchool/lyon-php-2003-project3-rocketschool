a6add486758bf5e7dcd28841fabd1b287efe42cc
fouhaha <francois.frk@gmail.com.com>
2020-07-07 11:01:21 +0200

    functional old users removal

---
+++ src/Command/DeleteOldAccountCommand.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use App\Repository\UserRepository;
+
+class DeleteOldAccountCommand extends Command
+{
+    // the name of the command (the part after "bin/console")
+    protected static $defaultName = 'app:delete-old-accounts';
+    private $userRepository;
+
+    protected function configure()
+    {
+        $this
+            // the short description shown while running "php bin/console list"
+            ->setDescription('Deletes old accounts.')
+
+            // the full command description shown when running the command with
+            // the "--help" option
+            ->setHelp('This command allows to delete all the user accounts older than 100 days.')
+        ;
+    }
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+
+        parent::__construct();
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->userRepository->deleteOldAccounts();
+
+        $output->writeln('Users successfully deleted!');
+
+        //  return Command::SUCCESS;
+        // return this if there was no problem running the command
+        // (it's equivalent to returning int(0))
+        // The Command::SUCCESS and Command::FAILURE constants were introduced in Symfony 5.1
+        return 0;
+    }
+}
--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -9,6 +9,8 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use DateTime;
 
 /**
  * @method User|null find($id, $lockMode = null, $lockVersion = null)
@@ -58,6 +60,52 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
         }
 
         return $query->getQuery()->getResult();
+    }
+
+    public function deleteOldAccounts()
+    {
+//        $query =
+
+        //$this->_em->createQueryBuilder('User')
+//        $query = $this->createQueryBuilder('User')
+//        ->delete('User', 'u')
+//        ->andWhere('u.createdAt <  NOW() - INTERVAL 100 DAY')
+//        ->getQuery()->execute();
+
+//        return $query->getQuery()->getResult();
+//        Ci-dessous : fonctionnel mais pas accepté par GrumPHP car format() retourne potentiellement false
+//        $now = new DateTime;
+//        $before = date_modify($now, '-100 days')->format('Y-m-d H:i:s');
+
+        $now = new DateTime();
+        //$before = date_modify($now, '-100 days')->format('Y-m-d H:i:s');
+        $before = date_modify($now, '-100 days');
+        //$qb->setParameter('date_from', $date_from, \Doctrine\DBAL\Types\Type::DATETIME);
+
+        $qb = $this->getEntityManager()->createQueryBuilder();
+            $qb->delete(User::class, 'u')
+                ->where("u.createdAt < :before")
+                ->setParameter('before', $before, \Doctrine\DBAL\Types\Types::DATETIME_MUTABLE)
+                ->getQuery()->execute();
+
+//        if ($before) {
+//            $qb = $this->getEntityManager()->createQueryBuilder();
+//            $qb->delete(User::class, 'u')
+//                ->where("u.createdAt < '$before' -100")
+//                ->getQuery()->execute();
+//        }
+        //$now->format('Y-m-d H:i:s');
+        //$before = date_modify($now, '-100 days');
+        //$qb = $this->getEntityManager()->createQueryBuilder();
+        //$qb->delete(User::class, 'u')
+        //->where("u.createdAt < '$now -100'")
+        //->where("u.createdAt < NOW() - INTERVAL 100 DAYS")
+        //->where("u.createdAt <  new DateTime(-100 days)")
+        //->where("u.createdAt <  $now->format('Y-m-d H:i:s') INTERVAL 100 DAY")
+        //->where("u.createdAt < date_format($now, 'Y-m-d H:i:s') INTERVAL 100 DAY")
+        // CI-DESSOUS ÇA MARCHE !!!
+        //->where("u.createdAt < '2019-08-06 17:32:53'")
+        //->getQuery()->execute();
     }
 
     // /**
