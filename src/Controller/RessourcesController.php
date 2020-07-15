7a8069bbfa47a5fee9fef93a4a9c8ff11cbb2302
galgros <galgros@yahoo.fr>
2020-07-15 16:11:31 +0200

    replace if empty user by else in admin index l127 & fix err 500 on category, ressourceController

--- src/Controller/RessourcesController.php
+++ src/Controller/RessourcesController.php
@@ -148,8 +148,10 @@ class RessourcesController extends AbstractController
 
         if ($form->isSubmitted() && !empty($form->getData())) {
             $data = $form->getData();
-            $faq = $this->faqRepository
-                ->findBySomeField($data['searchField'], $data['category']);
+            if (isset($data['category'])) {
+                $faq = $this->faqRepository
+                    ->findBySomeField($data['searchField'], $data['category']);
+            }
         }
 
         return $this->render('ressources/faq.html.twig', [
