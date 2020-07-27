ec46b498f87e87a4ce3dbf15e18334e24cfef769
galgros <galgros@yahoo.fr>
2020-07-27 10:28:44 +0200

    fixtures

--- src/DataFixtures/Prod/AccountsDurationFixtures.php
+++ src/DataFixtures/Prod/AccountsDurationFixtures.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\DataFixtures;
+namespace App\DataFixtures\Prod;
 
 use App\Entity\AccountsDuration;
 use App\Entity\Program;
--- src/DataFixtures/Prod/CategoryFixtures.php
+++ src/DataFixtures/Prod/CategoryFixtures.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\DataFixtures;
+namespace App\DataFixtures\Prod;
 
 use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
